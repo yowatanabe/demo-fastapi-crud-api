817dc58aef32826d917454aa6b3bf09928145f30
yowatanabe <57588645+yowatanabe@users.noreply.github.com>
2023-07-09 15:38:39 +0900

    #10 DELETE処理追加

--- app/main.py
+++ app/main.py
@@ -40,6 +40,14 @@ def read_user(user_id: int, db: Session = Depends(get_db)):
     return db_user
 
 
+@app.delete("/users/{user_id}", response_model=schemas.User)
+def delete_user(user_id: int, db: Session = Depends(get_db)):
+    db_user = crud.get_user(db, user_id=user_id)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return crud.delete_user(db=db, user_id=user_id)
+
+
 @app.post("/users/{user_id}/items/", response_model=schemas.Item)
 def create_item_for_user(
     user_id: int, item: schemas.ItemCreate, db: Session = Depends(get_db)
